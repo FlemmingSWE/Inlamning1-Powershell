f0640d4d775e9e39343846e138837babfbf1b820
Flamur Mehmeti <flamme_908@hotmail.com>
2026-05-13 18:02:41 +0200

    Function test of user input

--- inlamning1/strukturverktyg.ps1
+++ inlamning1/strukturverktyg.ps1
@@ -1,1 +1,12 @@
-Write-Host "Hello world!"
+# Asks the user for the desired map name
+
+
+function Map-Name {
+    param (
+        [string]$mapName
+    )
+    $mapName = Read-Host "Type the desired map name "
+    Write-Host "The desired map name is $mapName"
+}
+
+Map-Name
