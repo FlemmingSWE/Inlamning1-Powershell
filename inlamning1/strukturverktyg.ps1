a05651512044881e39e01e7ed505fcfeffe3bbac
Flamur Mehmeti <flamme_908@hotmail.com>
2026-05-15 14:31:22 +0200

    added a function that creates maps with a user desired name.

--- inlamning1/strukturverktyg.ps1
+++ inlamning1/strukturverktyg.ps1
@@ -1,12 +1,21 @@
 # Asks the user for the desired map name
-
-
 function Map-Name {
     param (
         [string]$mapName
     )
-    $mapName = Read-Host "Type the desired map name "
-    Write-Host "The desired map name is $mapName"
+    $mapName = Read-Host "Type the desired map name"
+    Write-Host "The desired map name is: $mapName"
+    return $mapName
 }
 
-Map-Name
+
+# Creates the map with the desired map name
+function Create-Map {
+    param (
+    )
+    $mapName = Map-Name
+    New-Item -Path "C:\Users\Flamur Mehmeti\Downloads\Scripting och automatisering inlämningsuppgift 1\Inlamning1-Powershell\inlamning1" -name "$mapName"  -ItemType "Directory"
+    
+}
+
+Create-Map
