d3147e6453f0d350d1575e9778db887914bd63d5
Flamur Mehmeti <flamme_908@hotmail.com>
2026-05-15 23:03:17 +0200

    added a function that creates subfolders

--- inlamning1/strukturverktyg.ps1
+++ inlamning1/strukturverktyg.ps1
@@ -1,21 +1,40 @@
 # Asks the user for the desired map name
-function Map-Name {
+function mapName {
     param (
-        [string]$mapName
+        [string]$name
     )
-    $mapName = Read-Host "Type the desired map name"
-    Write-Host "The desired map name is: $mapName"
-    return $mapName
+    $name = Read-Host "Type the desired map name"
+    Write-Host "The desired map name is: $name"
+    return $name
 }
 
 
 # Creates the map with the desired map name
-function Create-Map {
+function createMap {
     param (
+        [string]$directoryPath
     )
-    $mapName = Map-Name
-    New-Item -Path "C:\Users\Flamur Mehmeti\Downloads\Scripting och automatisering inlämningsuppgift 1\Inlamning1-Powershell\inlamning1" -name "$mapName"  -ItemType "Directory"
-    
+    $directoryPath = "C:\Users\Flamur Mehmeti\Downloads\Scripting och automatisering inlämningsuppgift 1\Inlamning1-Powershell\inlamning1"
+    $name = mapName
+    New-Item -Path $directoryPath -name $name  -ItemType Directory
+    $directoryPath = "C:\Users\Flamur Mehmeti\Downloads\Scripting och automatisering inlämningsuppgift 1\Inlamning1-Powershell\inlamning1\$name"
 }
 
-Create-Map
+Write-Host "directory: $directoryPath\$mapName"
+
+# Creates the subfolders "logs", "scripts" and "temp"
+function subFolders {
+    param (
+        [string]$directoryPath
+    )
+    $subFolderNames = @("logs", "scripts", "temp")
+    foreach ($mapName in $subFolderNames)
+    {
+        Write-Host "the directory is: $directoryPath\$mapName"
+        Write-Host "Creating item: $directoryPath\$mapName"
+        New-Item -Path $directoryPath -Name $mapName -ItemType "Directory"
+    }
+}
+
+
+subFolders -directoryPath (createMap)
